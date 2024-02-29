d2e35954343d418658b341a3ece4a038b1c4717b
Murilosgama <murilosgama@gmail.com>
2024-02-29 10:52:05 -0300

    salvo

---
+++ Calculadora/js/script.js
@@ -0,0 +1,43 @@
+function confNum1(){
+    var num1 = Number(document.getElementById('num1').value)
+}
+
+function confNum2(){
+    var num2 = Number(document.getElementById('num2').value)
+}
+
+function fazerAdi(){
+    var num1 = Number(document.getElementById('num1').value)
+    var num2 = Number(document.getElementById('num2').value)
+    var fazerAdi = num1 + num2
+    result.innerHTML = fazerAdi
+}
+
+function fazerSub(){
+    var num1 = Number(document.getElementById('num1').value)
+    var num2 = Number(document.getElementById('num2').value)
+    var fazerSub = num1 - num2
+    result.innerHTML = fazerSub
+}
+
+function fazerMult(){
+    var num1 = Number(document.getElementById('num1').value)
+    var num2 = Number(document.getElementById('num2').value)
+    var fazerMult = num1 * num2
+    result.innerHTML = fazerMult
+}
+
+function fazerDiv(){
+    var num1 = Number(document.getElementById('num1').value)
+    var num2 = Number(document.getElementById('num2').value)
+    var fazerDiv = num1 / num2
+    result.innerHTML = fazerDiv
+}
+
+function limpar(){
+    result.innerHTML = ' '
+    var num1 = Number(document.getElementById('num1').value)
+    var num2 = Number(document.getElementById('num2').value)
+    num1.innerHTML = value = ' '
+    num2.innerHTML = value = ' '
+}
