0e7f91e7e7cd1502ed49db0418956195c36e870b
Murilosgama <murilosgama@gmail.com>
2024-02-29 11:07:36 -0300

    salvo

--- Calculadora/js/script.js
+++ Calculadora/js/script.js
@@ -36,8 +36,4 @@ function fazerDiv(){
 
 function limpar(){
     result.innerHTML = ' '
-    var num1 = Number(document.getElementById('num1').value)
-    var num2 = Number(document.getElementById('num2').value)
-    num1.innerHTML = value = ' '
-    num2.innerHTML = value = ' '
 }
