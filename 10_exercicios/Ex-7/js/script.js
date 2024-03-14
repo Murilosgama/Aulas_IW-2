1bb3fc1205963fd2eeee54c6e41f5309087ab071
Murilosgama <murilosgama@gmail.com>
2024-03-14 19:23:48 -0300

    salvo

---
+++ 10_exercicios/Ex-7/js/script.js
@@ -0,0 +1,12 @@
+function botao(){
+    var nome = prompt('Digite seu nome: ')
+    nom.innerHTML = nome
+    var sala = prompt('Qual seu salário ' + nome + '?')
+    sala.innerHTML = sala
+    var rea = prompt(nome + ' em quantos porcento vai ser o seu reajuste?')
+    reaju.innerHTML = rea
+    var aumento = sala *(rea / 100);
+    aumen.innerHTML = aumento
+    var total = sala + aumento;
+    valuAtu.innerHTML = total
+}
