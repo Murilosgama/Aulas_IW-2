b64765070de6c00dced26be6a6d4feb42b3517fc
Murilosgama <murilosgama@gmail.com>
2024-03-28 09:01:01 -0300

    botôes

--- Manipulação_de_CSS/js/script.js
+++ Manipulação_de_CSS/js/script.js
@@ -26,23 +26,87 @@ let btnDark = document.querySelector('#btdark')
 
 let btnLight = document.querySelector('#btlight')
 
+let btnPink = document.querySelector('#btpink')
+
+let btnBlue = document.querySelector('#btblue')
+
+let btnYellow = document.querySelector('#btyellow')
+
+let btnGreen = document.querySelector('#btgreen')
+
 btnDark.addEventListener('click', modoDark)
 btnLight.addEventListener('click', modoLight)
+btnPink.addEventListener('click', modoPink)
+btnBlue.addEventListener('click', modoBlue)
+btnYellow.addEventListener('click', modoYellow)
+btnGreen.addEventListener('click', modoGreen)
 
 function modoDark() {
     event.preventDefault();
     console.log('modo dark')
-    tela.classList.add("dark");
     tela.classList.remove("light");
+    tela.classList.remove("pink");
+    tela.classList.remove("blue");
+    tela.classList.remove("yellow");
+    tela.classList.remove("green");
+    tela.classList.add("dark");
+    
 }
 
 function modoLight() {
     event.preventDefault();
     console.log('modo light')
     tela.classList.remove("dark");
+    tela.classList.remove("pink");
+    tela.classList.remove("blue");
+    tela.classList.remove("yellow");
+    tela.classList.remove("green");
     tela.classList.add("light");
 }
 
+function modoPink() {
+    event.preventDefault();
+    console.log('modo pink')
+    tela.classList.remove("dark");
+    tela.classList.remove("light");
+    tela.classList.remove("blue");
+    tela.classList.remove("yellow");
+    tela.classList.remove("green");
+    tela.classList.add("pink");
+}
+
+function modoBlue() {
+    event.preventDefault();
+    console.log('modo blue')
+    tela.classList.remove("dark");
+    tela.classList.remove("light");
+    tela.classList.remove("pink");
+    tela.classList.remove("yellow");
+    tela.classList.remove("green");
+    tela.classList.add("blue");
+}
+
+function modoYellow() {
+    event.preventDefault();
+    console.log('modo yellow')
+    tela.classList.remove("dark");
+    tela.classList.remove("light");
+    tela.classList.remove("pink");
+    tela.classList.remove("blue");
+    tela.classList.remove("green");
+    tela.classList.add("yellow");
+}
+
+function modoGreen() {
+    event.preventDefault();
+    console.log('modo yellow')
+    tela.classList.remove("dark");
+    tela.classList.remove("light");
+    tela.classList.remove("pink");
+    tela.classList.remove("blue");
+    tela.classList.remove("yellow");
+    tela.classList.add("green");
+}
 
 titulo.addEventListener('click', mudarId)
 function mudarId() {
