8836e39a5bd00e2c77b26345c8e674e920df8241
projetoscintia <cpinho@tecnologia-unica.com.br>
2024-05-20 11:32:52 -0300

    salvo

---
+++ 2_Bim/01-Hello_World/calculadora.js
@@ -0,0 +1,9 @@
+var SomarFunc = require("./modulo/somar")
+var SubFunc = require("./modulo/subtrair")
+var MultFunc = require("./modulo/multiplicar")
+var DivFunc = require("./modulo/dividir")
+
+console.log(SomarFunc(4,7))
+console.log(SubFunc(4,7))
+console.log(MultFunc(4,7))
+console.log(DivFunc(4,7))
--- 2° Bim/01-Hello_World/calculadora.js
+++
@@ -1,9 +0,0 @@
-var SomarFunc = require("./modulo/somar")
-var SubFunc = require("./modulo/subtrair")
-var MultFunc = require("./modulo/multiplicar")
-var DivFunc = require("./modulo/dividir")
-
-console.log(SomarFunc(4,7))
-console.log(SubFunc(4,7))
-console.log(MultFunc(4,7))
-console.log(DivFunc(4,7))
