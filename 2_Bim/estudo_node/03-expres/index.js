3866f77c9e424156b038a2f3b559603b3c866796
projetoscintia <cpinho@tecnologia-unica.com.br>
2024-05-20 12:32:15 -0300

    salvo

--- 2_Bim/estudo_node/03-expres/index.js
+++ 2_Bim/estudo_node/03-expres/index.js
@@ -5,6 +5,21 @@ app.get('/',function(req,res){
     res.send('Olá, como está você, seja bem vindo!!')
 })
 
-app.listen(0001,function(){
-    console.log('Servidor rodando em http://localhost.0001')
+app.get('/blog',function(req,res){
+    res.send('Olá, como está você, seja bem vindo ao nosso blog!!')
+})
+
+app.get('/sobre',function(req,res){
+    res.send('Olá, saiba tudo sobre nós!!')
+})
+
+app.get('/cadastro/:nome',function(req,res){
+    res.send('Olá, seu nome: ' + req.params.nome + 'sua idade:' + req.params.idade)
+    //res.send('Olá, sua idade: ' + req.params.idade)
+    //req.send(req.params)
+})
+
+
+app.listen(8081,function(){
+    console.log('Servidor rodando em http://localhost.8081')
 })
