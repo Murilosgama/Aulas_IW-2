f2e921fe1fc7b976d08b39b90484caf6c4fa8e46
Murilosgama <murilosgama@gmail.com>
2024-06-06 07:39:12 -0300

    salvar

---
+++ 2_Bim/estudo_node/04-html/index.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var app = express()
+
+app.get('/',function(req,res){
+    res.send('Olá, como está você, seja bem vindo!!')
+})
+
+app.get('/blog',function(req,res){
+    res.send('Olá, como está você, seja bem vindo ao nosso blog!!')
+})
+
+app.get('/sobre',function(req,res){
+    res.send('Olá, saiba tudo sobre nós!!')
+})
+
+app.get('/cadastro/:nome',function(req,res){
+    res.send('Olá, seu nome: ' + req.params.nome + '. Sua idade:' + req.params.idade)
+    //res.send('Olá, sua idade: ' + req.params.idade)
+    //req.send(req.params)
+})
+
+
+app.listen(8081,function(){
+    console.log('Servidor rodando em http://localhost.8081')
+})
